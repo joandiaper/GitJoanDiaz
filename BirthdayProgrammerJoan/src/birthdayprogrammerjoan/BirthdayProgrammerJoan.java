783468f7dc7260268474d9fce42ed804bec3a65d
Joan Angel Diaz Pernias <joandiaper@campus.monlau.com>
2021-02-16 09:23:23 +0100

    first commit

---
+++ BirthdayProgrammerJoan/src/birthdayprogrammerjoan/BirthdayProgrammerJoan.java
@@ -0,0 +1,31 @@
+/*
+Joan Diaz
+ASIX 1 Monlau
+16/02/2021
+ */
+package birthdayprogrammerjoan;
+
+public class BirthdayProgrammerJoan {
+
+    public static void main(String[] args) {
+        int Dia, Mes, Any;
+        Dia =13;
+        Mes = 9;
+        Any = 2021;
+
+        String date=inicioDiaMesZeller(Dia, Mes, Any);
+        
+        System.out.println("Day of the week: "+date+" Sep 2021");
+    }
+
+    private static String inicioDiaMesZeller(int day, int month, int year) {
+        String[] ArrayWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        
+        int a, y, m, d;
+        a = (14 - month) / 12;
+        y = year - a;
+        m = month + 12 * a - 2;
+        d = (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
+        return ArrayWeek[d];
+    }
+}
